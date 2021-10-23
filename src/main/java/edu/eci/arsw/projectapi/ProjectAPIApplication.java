619916570994f840c708a4fd008197d3e81eb92f
Juan Alayon <jkam86425@gmail.com>
2021-10-23 18:59:42 -0500

    Hibernate config

--- src/main/java/edu/eci/arsw/projectapi/ProjectAPIApplication.java
+++ src/main/java/edu/eci/arsw/projectapi/ProjectAPIApplication.java
@@ -1,16 +1,18 @@
 package edu.eci.arsw.projectapi;
 
+import edu.eci.arsw.project.persistence.StoreData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration("/applicationContext.xml")
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"edu.eci.arsw.project"})
 public class ProjectAPIApplication {
 
     public static void main(String[] args) {
+        StoreData.makeBD();
         SpringApplication.run(ProjectAPIApplication.class, args);
     }
 }
