3e194287dabf1a6bc0b802e6a660497db535bc4b
Brayan Macias <brayan.macias@escuelaing.edu.co>
2021-10-22 23:10:44 -0500

    Change on java Test Class

--- src/test/java/edu/eci/arsw/project/ProjectApplicationTests.java
+++ src/test/java/edu/eci/arsw/project/ProjectApplicationTests.java
@@ -1,9 +1,11 @@
 package edu.eci.arsw.project;
 
+import edu.eci.arsw.project.controllers.ProjectAPIController;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProjectAPIController.class)
 class ProjectApplicationTests {
 
 	@Test
