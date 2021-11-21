bb9c9e5a6fa8bca11abed2d20e87294711aa9f05
Juan Alayon <jkam86425@gmail.com>
2021-11-21 16:50:28 -0500

    fixed error

--- src/main/resources/static/js/apiclient.js
+++ src/main/resources/static/js/apiclient.js
@@ -1,21 +1,3 @@
 var apiclientAPI = (function () {
-    return {
-        getAllUsersAPI: function (callback) {
-            console.log("Entra a apiclient")
-            $.get(
-                {
-                    url: "/project/users/",
-                    contentType: "application/json",
-                }
-                , function (data){
-                    console.log("Obtencion de datos")
-                    console.log(data);
-                    callback(null, data);
-                }).fail(function (error) {
-                    alert("No existen los datos")
-                }
-            );
 
-        }
-    }
 })
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,11 +1,4 @@
 var appAPI = ( function () {
 
-    return {
-        getUsers: function () {
-            apiclientAPI.getAllUsersAPI(function (error,data){
-                console.log(data);
-                console.log("Datos Obtenidos");
-            })
-        }
-    }
+
 })
